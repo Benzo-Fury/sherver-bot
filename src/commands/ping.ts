590fc32fbf97180d39ff80e99b362dbf8f29cf86
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2023-03-03 14:20:55 +1000

    refractor: changed default command from both to slash

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,10 +1,9 @@
 import { commandModule, CommandType } from '@sern/handler';
 
 export default commandModule({
-	type: CommandType.Both,
+	type: CommandType.Slash,
 	plugins: [],
 	description: 'A ping command',
-	//alias : [],
 	execute: async (ctx, args) => {
 		await ctx.reply('Pong 🏓');
 	},
