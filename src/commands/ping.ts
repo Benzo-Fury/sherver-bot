c0128f61275047a8ba6c863d9403a3a9027ac1f0
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2023-03-03 14:33:31 +1000

    added publish plugin to ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,8 +1,9 @@
 import { commandModule, CommandType } from '@sern/handler';
+import { publish } from '../plugins/publish'
 
 export default commandModule({
 	type: CommandType.Slash,
-	plugins: [],
+	plugins: [publish()],
 	description: 'A ping command',
 	execute: async (ctx, args) => {
 		await ctx.reply('Pong 🏓');
