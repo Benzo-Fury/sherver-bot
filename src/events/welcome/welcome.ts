61cb2aed7b7923383f136b88f258cc5b67f00349
NotOinez <90028564+NotOinez@users.noreply.github.com>
2023-03-04 17:04:32 +1100

    test code for chatgpt welcome

--- src/events/welcome/welcome.ts
+++ src/events/welcome/welcome.ts
@@ -1,8 +1,18 @@
-export default eventModule({
-    type: EventType.Discord,
-    plugins : []],
-    name: 'guildMemberAdd', //name of event.
-    async execute(member: GuildMember) {
-      (await member.guild.channels.fetch('1080053561863442464') as TextChannel).send(`Welcome, ${member}`); 
-    }
-  })
+import { GuildMember, TextChannel } from 'discord.js';
+
+const welcomeMessages = [
+  'Welcome to our server! We hope you have a great time here!',
+  'Hey there, welcome to the community!',
+  'We\'re glad you\'re here! Welcome to our server!',
+  'Welcome, friend! We hope you enjoy your stay with us!'
+];
+
+export default function welcomeNewMember(member: GuildMember) {
+  // Get a random welcome message from the array
+  const randomIndex = Math.floor(Math.random() * welcomeMessages.length);
+  const randomMessage = welcomeMessages[randomIndex];
+  
+  // Send the message to a specific channel (replace CHANNEL_ID with your desired channel ID)
+  const channel = member.guild.channels.cache.get('CHANNEL_ID') as TextChannel;
+  channel.send(`${randomMessage} ${member.user.username}!`);
+}
