4a74f8676532d36a5093bd865cc35c81374d12eb
NotOinez <90028564+NotOinez@users.noreply.github.com>
2023-03-04 16:52:47 +1100

    welcome.ts added channel id

--- src/events/welcome/welcome.ts
+++ src/events/welcome/welcome.ts
@@ -3,7 +3,6 @@ export default eventModule({
     plugins : []],
     name: 'guildMemberAdd', //name of event.
     async execute(member: GuildMember) {
-      (await member.guild.channels.fetch('channel-id') as TextChannel).send(`Welcome, ${member}`); 
+      (await member.guild.channels.fetch('1080053561863442464') as TextChannel).send(`Welcome, ${member}`); 
     }
   })
-  
