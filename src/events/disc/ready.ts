aaee102f849fcf6d37eda8ef92a1dd2bd0e3aed6
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2023-03-04 12:09:46 +1000

    feat: added status's

--- src/events/disc/ready.ts
+++ src/events/disc/ready.ts
@@ -1,6 +1,6 @@
 import { eventModule, EventType } from "@sern/handler";
 import type { Client } from "discord.js";
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,8 +11,22 @@ export default eventModule({
   name: "ready", //name of event.
   async execute(client: Client) {
     //connecting to mongodb db
-    await mongoose.connect(process.env.MONGOURI || '', {
-        keepAlive: true
-    })
+    await mongoose.connect(process.env.MONGOURI || "", {
+      keepAlive: true,
+    });
+    //running status's
+    var status = [
+      "wif Molly.",
+      "with Nwo.",
+      "with yaw mumma.",
+      "I'm hewe to he'p you!",
+      "I'm a bot with a lisp, thith ith how I talk!",
+      "I'm always weady to make new fwiends on Discord!",
+    ];
+    setInterval(() => {
+      client.user?.setActivity(
+        status[Math.floor(Math.random() * status.length)]
+      );
+    }, 10000);
   },
 });
