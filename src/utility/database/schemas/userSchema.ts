589e0aace5dac412788e223017f51705191fc93f
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2023-03-07 21:38:12 +1000

    fix: schema appeal support

--- src/utility/database/schemas/userSchema.ts
+++ src/utility/database/schemas/userSchema.ts
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
   level: {
     type: Number,
     required: true
+  },
+  appealed: {
+    type: Boolean,
+    required: true,
+    default: false
   }
 });
 
