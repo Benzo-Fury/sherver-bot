fc53aa5f2668f52eef8e61fa3e9f16324e397dd4
Neo <80315475+Benzo-Fury@users.noreply.github.com>
2023-03-11 09:37:26 +1000

    feat: changelog

--- src/commands/admin/changelog.ts
+++ src/commands/admin/changelog.ts
@@ -1,35 +1,44 @@
-import { commandModule, CommandType } from '@sern/handler';
-import { ApplicationCommandOptionType, EmbedBuilder, TextChannel } from 'discord.js';
-import { publish } from '../../plugins/publish'
-import { requirePermission } from '../../plugins/requirePermission';
+import { commandModule, CommandType } from "@sern/handler";
+import {
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  TextChannel,
+} from "discord.js";
+import { publish } from "../../plugins/publish";
+import { requirePermission } from "../../plugins/requirePermission";
 
 export default commandModule({
-    type: CommandType.Slash,
-    plugins: [publish(), requirePermission('user', ['Administrator'], 'You are not allowed to use that')],
-    description: 'Creates a new changelog.',
-    options: [
-        {
-            name: "version",
-            description: "The version of this changelog.",
-            type: ApplicationCommandOptionType.String,
-            required: true
-        },
-        {
-            name: "description",
-            description: "The contents of this changelog.",
-            type: ApplicationCommandOptionType.String,
-            required: true
-        }
-    ],
-    execute: async (ctx) => {
-        const version = ctx.interaction.options.getString('version')!;
-        const desc = ctx.interaction.options.getString('description')
-        const embed = new EmbedBuilder()
-        .setColor("#1f76cc")
-        .setAuthor({name: `Update | ${version}`, iconURL: ctx.client.user?.displayAvatarURL()})
-        .setDescription(desc)
-        .setFooter({text: 'This update has been released at'})
-        .setTimestamp()
-        await (ctx.channel as TextChannel).send({embeds: [embed]}) //TODO: Ping changelog ping here
-    },
+  type: CommandType.Text,
+  plugins: [
+    requirePermission(
+      "user",
+      ["Administrator"],
+      "You are not allowed to use that"
+    ),
+  ],
+  description: "Creates a new changelog.",
+  execute: async (ctx, args) => {
+    const version = args[1][0]
+    const desc = ctx.message.content.replace(`)changelog ${args[1][0]} `, '')
+    if (!version) {
+      return await ctx.reply({
+        content: "Specify a version.",
+        ephemeral: true,
+      });
+    }
+    if (!desc) {
+      return await ctx.reply({ content: "Specify a desc.", ephemeral: true });
+    }
+    await ctx.message.delete()
+    const embed = new EmbedBuilder()
+      .setColor("#1f76cc")
+      .setAuthor({
+        name: `Release | ${version}`,
+        iconURL: ctx.client.user?.displayAvatarURL(),
+      })
+      .setDescription(desc)
+      .setFooter({ text: "This update has been released:" })
+      .setTimestamp();
+    await (ctx.channel as TextChannel).send({ embeds: [embed] }); //TODO: Ping changelog ping here
+  },
 });
